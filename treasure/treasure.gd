f7b13600b351be23b81948bf0d22fd12ecadc1a7
Konstantin Klepikov <oformleno@gmail.com>
2023-06-15 02:34:11 +0200

    treasure script

---
+++ treasure/treasure.gd
@@ -0,0 +1,16 @@
+extends Area2D
+@export var treasure: int = 1000
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	treasure += 100
+	if treasure < 10000:
+		$GoldHeapq1.visible = true
+		$GoldHeapq2.visible = false
+	elif treasure > 10000 and treasure < 100000:
+		$GoldHeapq1.visible = false
+		$GoldHeapq2.visible = true
