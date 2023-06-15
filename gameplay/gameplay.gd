142440100c2b5f4764173bdae313c9ebfb892104
Konstantin Klepikov <oformleno@gmail.com>
2023-06-15 04:27:26 +0200

    treasure add

--- gameplay/gameplay.gd
+++ gameplay/gameplay.gd
@@ -1,12 +1,19 @@
 extends Node2D
 
-func _process(_delta: float) -> void:
+
+func _process(delta: float) -> void:
+	# treasure score
+	$Treasure.treasure += 100
+	$HUD.update_treasure_label($Treasure.treasure)
+
 	if Input.is_action_pressed("ui_accept"):
 		get_tree().change_scene_to_file(Global.SCENE_MAIN_MENU)
+
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_released("pause"):
 		call_deferred("_pause")
+
 
 func _pause() -> void:
 	$Paused.pause()
--- treasure/treasure.gd
+++ treasure/treasure.gd
@@ -1,13 +1,15 @@
 extends Area2D
 @export var treasure: int = 1000
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	treasure += 100
+func _process(delta: float) -> void:
+
 	if treasure < 10000:
 		$GoldHeapq1.visible = true
 		$GoldHeapq2.visible = false
