c67e00d85341a7a1c2ed293fc587cb35a3d2eda5
Konstantin Klepikov <oformleno@gmail.com>
2023-06-30 03:14:26 +0200

    scenes tilests

--- gameplay/gameplay.gd
+++ gameplay/gameplay.gd
@@ -1,11 +1,17 @@
 extends Node2D
+@export var money: int = 1000
+@onready var treasure = get_node("/root/Treasure")
+
+
+# func _ready() -> void:
+# 	treasure.instance()
 
 
 func _process(delta: float) -> void:
 	# treasure score
-	$Treasure.treasure += 100
-	$HUD.update_treasure_label($Treasure.treasure)
-	$Treasure.update_treasure_label($Treasure.treasure)
+	money += 100
+	$HUD.update_treasure_label(money)
+	treasure.update_treasure_label(money)
 
 	if Input.is_action_pressed("ui_accept"):
 		get_tree().change_scene_to_file(Global.SCENE_MAIN_MENU)
