5c3959aa11d2cb3bdc77d585682b71ea139149ea
Konstantin Klepikov <oformleno@gmail.com>
2023-07-01 01:27:23 +0200

    scoring and objects fixes

--- gameplay/gameplay.gd
+++ gameplay/gameplay.gd
@@ -1,17 +1,16 @@
 extends Node2D
 @export var money: int = 1000
-@onready var treasure = get_node("/root/Treasure")
-
-
-# func _ready() -> void:
-# 	treasure.instance()
 
 
 func _process(delta: float) -> void:
+
+	if $HUD.visible == false:
+		$HUD.show()
+
 	# treasure score
 	money += 100
 	$HUD.update_treasure_label(money)
-	treasure.update_treasure_label(money)
+	$Treasure.update_treasure_label(money)
 
 	if Input.is_action_pressed("ui_accept"):
 		get_tree().change_scene_to_file(Global.SCENE_MAIN_MENU)
@@ -24,4 +23,5 @@ func _input(event: InputEvent) -> void:
 
 func _pause() -> void:
 	$Paused.pause()
+	$HUD.hide()
 	get_tree().paused = true
