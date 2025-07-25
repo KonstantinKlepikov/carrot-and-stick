bd6ee1bf902586e02fbc420a41e9ffcb4c5a3bd5
Konstantin Klepikov <oformleno@gmail.com>
2025-07-25 05:51:44 +0300

    refactoring, reinit

--- gameplay/gameplay.gd
+++ gameplay/gameplay.gd
@@ -2,18 +2,10 @@ extends Node2D
 @export var money: int = 1000
 
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 
 	if $HUD.visible == false:
 		$HUD.show()
-
-	# treasure score
-	money += 100
-	$HUD.update_treasure_label(money)
-	$Treasure.update_treasure_label(money)
-
-	if Input.is_action_pressed("ui_accept"):
-		get_tree().change_scene_to_file(Global.SCENE_MAIN_MENU)
 
 
 func _input(event: InputEvent) -> void:
